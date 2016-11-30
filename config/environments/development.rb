a0a66f3ec42483cb5b4d90a51d8f421c22b906cb
simplegr33n <gcgolda@gmail.com>
2016-11-29 18:41:03 -0800

    fixed conf email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
  config.action_mailer.delivery_method = :smtp
 
  config.action_mailer.smtp_settings = {
-  adress: 'smtp.sendgrid.net',
+  address: 'smtp.sendgrid.net',
   port: '587',
   enable_starttls_auto: true,
   user_name: 'gcgolda',
