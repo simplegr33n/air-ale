38e7b56a5ec2f8bf66b4351995ebd0d348118308
simplegr33n <gcgolda@gmail.com>
2016-11-30 19:24:02 -0800

    added google auth

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'sendgrid-rails', '~> 2.0', '>= 2.0.5'
 gem 'omniauth'
 gem 'omniauth-facebook'
 
+gem 'omniauth-google-oauth2'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,4 +275,7 @@ Devise.setup do |config|
 
   # see config/application.yml
   config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_key'], scope: 'email', info_fields: 'email,name'
+
+  config.omniauth :google_oauth2, ENV['google_client_id'], ENV['google_client_secret']
+
 end
