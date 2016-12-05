7c69757456537d9066896997ac14efb62baa4db0
simplegr33n <gcgolda@gmail.com>
2016-12-04 23:45:12 -0800

    tried including conflict checker for datepicker... conflicts still possible

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,22 +1,40 @@
 class ReservationsController < ApplicationController
-  before_action :authenticate_user!
+	before_action :authenticate_user!
 
-  def preload
-  	room = Room.find(params[:room_id])
-  	today = Date.today
-  	reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+	def preload
+		room = Room.find(params[:room_id])
+		today = Date.today
+		reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
 
-  	render json: reservations
-  end
+		render json: reservations	
+	end
 
-  def create
-    @reservation = current_user.reservations.create(reservation_params)
+	def preview
+		start_date = Date.parse(params[:start_date])
+		end_date = Date.parse(params[:end_date])
 
-    redirect_to @reservation.room, notice: "Your reservation has been created!"
-  end
+		output = {
+			conflict: is_conflict(start_date, end_date)
+		}
 
-  private
-  	def reservation_params
-  	  params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
-  	end
+		render json: output
+	end
+
+	def create
+		@reservation = current_user.reservations.create(reservation_params)
+
+		redirect_to @reservation.room, notice: "Your reservation has been created..."
+	end
+
+	private
+		def is_conflict(start_date, end_date)
+			room = Room.find(params[:room_id])
+
+			check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+			check.size > 0? true : false
+		end
+
+		def reservation_params
+			params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   end
 
   get '/preload' => 'reservations#preload'
+  get '/preview' => 'reservations#preview'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
