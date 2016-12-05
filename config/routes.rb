ed3050d8a29800f01b87ed92bf82a4899f0d80cc
simplegr33n <gcgolda@gmail.com>
2016-12-04 22:06:03 -0800

    added AJAX for start-date

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
     resources :reservations, only: [:create]
   end
 
+  get '/preload' => 'reservations#preload'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
