49b00bd2b86464e3bed0ad52f1c1976aa4976b13
simplegr33n <gcgolda@gmail.com>
2016-12-05 13:02:51 -0800

    added your_reservations page

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   get '/preview' => 'reservations#preview'
 
   get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
